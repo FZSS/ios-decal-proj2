8dc90105975a40f06050a669ae747ab8159eb4d3
Kevin Fangzhou Chen <kevin@Fangzhous-MacBook-Pro.local>
2016-03-18 05:11:47 -0700

    starting over is working

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var GuessTextUI: UILabel!
+    @IBOutlet weak var InputTextField: UITextField!
+    @IBOutlet weak var GuessButton: UIButton!
+    @IBOutlet weak var HangmanImage: UIImageView!
+    @IBOutlet weak var StartOverButton: UIButton!
+    
+    var secretWord : String = ""
+    var incorrectTryNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
-        print(phrase)
+        secretWord = hangmanPhrases.getRandomPhrase()
+        var length :  Int = secretWord.characters.count
+        var dashes = ""
+        for _ in 0..<length {
+            dashes += "-"
+        }
+        GuessTextUI.text = dashes
+        loadInterface()
+    }
+    
+    
+    func loadInterface() {
+        GuessButton.addTarget(self, action:"guess", forControlEvents: .TouchUpInside)
+        StartOverButton.addTarget(self, action:"startOver", forControlEvents: .TouchUpInside)
     }
 
+    func startOver() {
+        viewDidLoad()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
